5339b54dda8b59f4321f16357540c7f528a84529
Никита Демирев <nikdem00@gmail.com>
2024-11-19 20:20:22 +0300

    c++ 20

--- ConsoleGame/main.cpp
+++ ConsoleGame/main.cpp
@@ -1,6 +1,19 @@
 #include "resources.h"
 #include "Game.h"
-extern void switch_window(MH_Points&, STATS&, Equip&, TConsole);
+
+void switch_window(PlayerGUI& GUI, TConsole Console) {
+	if (((GUI.mphp_points.hiden or GUI.stats.hiden or (not GUI.equip.hiden))) == 0) {
+		GUI.mphp_points.hide(Console);
+		GUI.stats.hide(Console);
+		GUI.equip.draw_full(Console);
+	}
+	else {
+		GUI.equip.hide(Console);
+		GUI.mphp_points.draw(Console);
+		GUI.stats.draw(Console);
+	}
+
+}
 
 int main() {
 
